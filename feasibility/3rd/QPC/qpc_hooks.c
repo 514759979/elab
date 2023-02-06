7a48f9c4af3742dfb89f2fc3d56a0641b7e8ca98
gouge <event-os@outlook.com>
2023-02-06 12:18:46 +0800

    Add QPC.

---
+++ feasibility/3rd/QPC/qpc_hooks.c
@@ -0,0 +1,36 @@
+#include "qpc.h"
+
+void QV_onIdle(void)
+{
+    QF_INT_ENABLE();
+}
+
+void QK_onIdle(void)
+{
+    QF_INT_ENABLE();
+}
+
+void QXK_onIdle(void)
+{
+
+}
+
+void QF_onStartup(void)
+{
+
+}
+
+void QF_onCleanup(void)
+{
+
+}
+
+char *p_module;
+uint32_t p_loc;
+void Q_onAssert(char_t const * const module, int const loc)
+{
+    p_module = (char *)module;
+    p_loc = loc;
+    
+    while (1);
+}
