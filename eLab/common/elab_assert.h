f2959a083dfaec56833ccdc16d89b07bc887dadf
gouge <event-os@outlook.com>
2023-02-07 08:44:13 +0800

    Add elab_assert and elab_common.

---
+++ eLab/common/elab_assert.h
@@ -0,0 +1,23 @@
+/*
+ * eLesson Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
+#ifndef ELAB_ASSERT_H
+#define ELAB_ASSERT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* public define ------------------------------------------------------------ */
+#define ELAB_TAG(tag)
+#define elab_assert(ex)                     ((void)0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* ----------------------------- end of file -------------------------------- */
---
+++ eLab/common/elab_common.h
@@ -0,0 +1,25 @@
+/*
+ * eLesson Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
+#ifndef ELAB_COMMON_H
+#define ELAB_COMMON_H
+
+/* includes ----------------------------------------------------------------- */
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* public typedef ----------------------------------------------------------- */
+uint32_t elab_time_ms(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* ----------------------------- end of file -------------------------------- */
