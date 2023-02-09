5ea96d2fa5865209a37c209f670be5efc32ff5fe
gouge <event-os@outlook.com>
2023-02-09 08:18:02 +0800

    Add the general elab header file.

---
+++ eLab/common/elab.h
@@ -0,0 +1,15 @@
+/*
+ * eLab Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
+#ifndef ELAB_H
+#define ELAB_H
+
+/* includes ----------------------------------------------------------------- */
+#include "elab_export.h"
+#include "elab_common.h"
+
+#endif
+
+/* ----------------------------- end of file -------------------------------- */
