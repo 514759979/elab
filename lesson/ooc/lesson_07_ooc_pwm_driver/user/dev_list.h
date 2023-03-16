6ee8d102c3d73b5c3cf04f6e24531a5b60d60d94
GouGe <event-os@outlook.com>
2023-03-16 19:51:15 +0800

    Lesson 7 added.

---
+++ lesson/ooc/lesson_07_ooc_pwm_driver/user/dev_list.h
@@ -0,0 +1,15 @@
+/*
+ * eLab Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
+#ifndef __DEV_LIST_H__
+#define __DEV_LIST_H__
+
+#include "eio_pwm.h"
+
+extern eio_pwm_t *led;
+
+#endif /* __DEV_LIST_H__ */
+
+/* ----------------------------- end of file -------------------------------- */
