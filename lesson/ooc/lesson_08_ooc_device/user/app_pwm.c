ddd3f7ac8ee8a377f9e01d83e434c829c110271d
GouGe <event-os@outlook.com>
2023-03-18 13:46:34 +0800

    Lesson 08 Added.

---
+++ lesson/ooc/lesson_08_ooc_device/user/app_pwm.c
@@ -0,0 +1,65 @@
+
+/*
+ * eLesson Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
+/* includes ----------------------------------------------------------------- */
+#include <stdlib.h>
+#include "eio_object.h"
+#include "eio_pwm.h"
+#include "elab.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* private config ----------------------------------------------------------- */
+#define LED_POLL_PERIOD_MS                  (5)
+
+/* private variables -------------------------------------------------------- */
+eio_object_t *led3 = NULL;
+eio_object_t *led4 = NULL;
+
+/* includes ----------------------------------------------------------------- */
+/**
+  * @brief  LED initialization.
+  * @retval None
+  */
+void led_init(void)
+{
+    led3 = eio_find("pwmled3");
+    elab_assert(led3 != NULL);
+    
+    led4 = eio_find("pwmled4");
+    elab_assert(led4 != NULL);
+}
+INIT_COMPONENT_EXPORT(led_init);
+
+/**
+  * @brief  LED polling function.
+  * @retval None
+  */
+void led_poll(void)
+{
+    uint16_t ms = elab_time_ms() % 1000;
+    uint8_t duty_ratio;
+    if (ms <= 500)
+    {
+        duty_ratio = ms / 5;
+    }
+    else
+    {
+        duty_ratio = (1000 - ms) / 5;
+    }
+
+    eio_pwm_set_duty(led3, duty_ratio);
+    eio_pwm_set_duty(led4, duty_ratio);
+}
+POLL_EXPORT(led_poll, LED_POLL_PERIOD_MS);
+
+#ifdef __cplusplus
+}
+#endif
+
+/* ----------------------------- end of file -------------------------------- */
