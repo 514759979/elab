f2abafd0898aca45965a297a02d004fb5aee5664
GouGe <event-os@outlook.com>
2023-05-05 13:19:54 +0800

    eLab midware esh updated.

--- eLab/midware/esh/esh.c
+++ eLab/midware/esh/esh.c
@@ -1,6 +1,13 @@
 
+/*
+ * eLab Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
+/* include ------------------------------------------------------------------ */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #if defined(__linux__)
 #include <termios.h>
 #include <unistd.h>
@@ -9,65 +16,210 @@
 #include <conio.h>
 #endif
 #include "esh.h"
+#include "elab_common.h"
+#include "elab_assert.h"
 
+ELAB_TAG("esh");
 
-#define EXSH_KEY_REG_TABLE          (32)
+/* private config ----------------------------------------------------------- */
+#define ESH_KEY_REG_TABLE           (32)
 
-#define KEY_ENTER                   (0x0a)          // Value of the Enter key
-#define KEY_ESC                     (0x1B)          // Value of the ESC key
-#define KEY_CTRL_C                  (0x03)          // Value of the CTRL+C key
-#define KEY_Fn                      (0x00)          // The 1st byte of F1 - F12
+/* private define ----------------------------------------------------------- */
+#define KEY_ENTER                   (0x0a)          /* Value of the Enter key */
+#ifdef __linux__
+#define KEY_ESC                     (0x03)
+#else
+#define KEY_ESC                     (0x1B)          /* Value of the ESC key */
+#endif
+#define KEY_CTRL_C                  (0x03)          /* Value of the CTRL+C key */
+#define KEY_Fn                      (0x00)          /* The 1st byte of F1 - F12 */
+#ifdef __linux__
+#define KEY_FUNCTION                (0x1B)
+#else
 #define KEY_FUNCTION                (0xE0)
-#define KEY_BACKSPACE               (0x08)          // Value of the Backspace key
+#endif
+#define KEY_BACKSPACE               (0x08)          /* Value of the Backspace key */
+#define KEY_TABLE                   (0x09)          /* Value of the Table key */
 
-typedef struct exsh_key_register
+/* private typedef ---------------------------------------------------------- */
+typedef struct esh_key_register
 {
     uint8_t key_id;
-    exsh_func_t func;
+    esh_func_t func;
     void *para;
-} exsh_key_register_t;
+} esh_key_register_t;
 
-typedef struct exsh
+typedef struct esh
 {
-    exsh_key_parser_t func_parser;
-    exsh_func_t func_normal;
-    exsh_func_exit_t func_exit;
-    void *nomarl_para;
+    esh_key_parser_t func_parser;
+    esh_key_register_t key_reg_table[Esh_Max - Esh_Null];
+    bool init_end;
+} esh_t;
 
-    exsh_key_register_t key_reg_table[Esh_Max - Esh_Null];
-} exsh_t;
+#if defined(__linux__)
+#include "cmsis_os.h"
 
-static exsh_t exsh;
+static int getch(void);
+static void _entry_getch(void *parameter);
 
-void ex_shell_init(exsh_key_parser_t func_parser,
-                   exsh_func_t func,
-                   exsh_func_exit_t func_exit,
-                   void *paras)
+/**
+ * @brief  The thread attribute for testing.
+ */
+static const osThreadAttr_t thread_attr_getch = 
 {
-    exsh.func_normal = func;
-    exsh.nomarl_para = paras;
-    exsh.func_parser = func_parser;
-    exsh.func_exit = func_exit;
+    .name = "ThreadGetChar",
+    .attr_bits = osThreadDetached,
+    .priority = osPriorityNormal,
+    .stack_size = 2048,
+};
 
-    for (uint32_t i = 0; i < EXSH_KEY_REG_TABLE; i ++)
+static osMessageQueueId_t mq_getch = NULL;
+#endif
+
+/* private variables -------------------------------------------------------- */
+static esh_t esh =
+{
+    .init_end = false,
+};
+
+/* public function ---------------------------------------------------------- */
+/**
+  * @brief  Esh F-key (esh_key_id) parser function type.
+  * @param  func_parser the F-key parser function.
+  * @retval None
+  */
+void esh_init(esh_key_parser_t func_parser)
+{
+    esh.func_parser = func_parser;
+
+    for (uint32_t i = 0; i < ESH_KEY_REG_TABLE; i ++)
     {
-        exsh.key_reg_table[i].key_id = Esh_Null;
-        exsh.key_reg_table[i].func = NULL;
-        exsh.key_reg_table[i].para = NULL;
+        esh.key_reg_table[i].key_id = Esh_Null;
+        esh.key_reg_table[i].func = NULL;
+        esh.key_reg_table[i].para = NULL;
     }
+
+#if defined(__linux__)
+    /*  On Linux, one specific thread is created for the function getch which 
+        can check the data is timeout or not. */
+    osThreadId_t _thread = osThreadNew(_entry_getch, NULL, &thread_attr_getch);
+    assert(_thread != NULL);
+#endif
+
+    esh.init_end = true;
 }
 
-void ex_shell_key_register(uint8_t key, exsh_func_t func, void *paras)
+/**
+  * @brief  Esh F-key function registering.
+  * @param  key     f-key ID.
+  * @param  func    Ther F-key function.
+  * @param  paras   Parameters.
+  * @retval None
+  */
+void esh_key_register(uint8_t key, esh_func_t func, void *paras)
 {
     uint32_t i = key - Esh_Null;
 
-    exsh.key_reg_table[i].key_id = key;
-    exsh.key_reg_table[i].func = func;
-    exsh.key_reg_table[i].para = paras;
+    esh.key_reg_table[i].key_id = key;
+    esh.key_reg_table[i].func = func;
+    esh.key_reg_table[i].para = paras;
 }
 
+/**
+  * @brief  Esh getch function.
+  * @param  None
+  * @retval key id.
+  */
+char esh_getch(void)
+{
+    char key_id;
 #if defined(__linux__)
-static int getch(void)
+    char buffer[16];
+    uint32_t count_ch = 0;
+#endif
+
+    /* If esh is not initialed, prevent the code enter assert. */
+    if (!esh.init_end)
+    {
+        osDelay(100);
+        key_id = Esh_Null;
+        goto exit;
+    }
+
+    while (esh.init_end)
+    {
+        key_id = getch();
+#ifdef __linux__
+        if (key_id < 0)
+        {
+            if (count_ch == 0)
+            {
+                continue;
+            }
+        }
+        if (key_id > 0)
+        {
+            buffer[count_ch ++] = key_id;
+            // continue;
+        }
+#endif
+
+        key_id = buffer[0];
+
+        if (key_id == KEY_ENTER)
+        {
+            key_id = '\n';
+            break;
+        }
+        /* Ctrl + A -> Z. */
+        else if (key_id >= 0x01 && key_id <= 0x1A &&
+                    key_id != KEY_CTRL_C && key_id != KEY_TABLE &&
+                    KEY_ESC != key_id)
+        {
+            /* Ignore the key Ctrl + A -> Z. */
+        }
+        /*
+        else if ((KEY_FUNCTION == key_id) || (KEY_Fn == key_id))
+        {
+#ifndef __linux__
+            buffer[1] = getch();
+            count_ch = 2;
+#endif
+            uint8_t key_func = esh.func_parser(buffer, count_ch);
+            uint8_t index = key_func - Esh_Null;
+            void *para = esh.key_reg_table[index].para;
+            if (esh.key_reg_table[index].func != NULL)
+            {
+                esh.key_reg_table[index].func(key_func, para);
+            }
+
+            count_ch = 0;
+            memset(buffer, 0, 16);
+
+            continue;
+        }
+        */
+        else
+        {
+            break;
+        }
+    }
+
+    count_ch = 0;
+    memset(buffer, 0, 16);
+
+exit:
+    return key_id;
+}
+
+/* private function --------------------------------------------------------- */
+#if defined(__linux__)
+/**
+  * @brief  The original __getch function for Linux which can get the input char
+  *         in the terminal.
+  * @retval Key id.
+  */
+static int __getch(void)
 {
     int ch;
 
@@ -81,48 +233,45 @@ static int getch(void)
 
     return ch;
 }
-#endif
 
-char esh_getch(void)
+/**
+  * @brief  The thread entry function for getch function. The thread is for 
+  *         checking the char is timeout or not. 
+  * @retval None.
+  */
+static void _entry_getch(void *parameter)
 {
-    char key_id;
+    mq_getch = osMessageQueueNew(16, 1, NULL);
+    assert(mq_getch != NULL);
 
     while (1)
     {
-        key_id = getch();
-        if ((KEY_ESC == key_id))
-        {
-            exsh.func_exit(exsh.nomarl_para);
-        }
-        else if (key_id == KEY_ENTER)
-        {
-            key_id = '\n';
-            break;
-        }
-        /* Ctrl + A -> Z. */
-        else if (key_id >= 0x01 && key_id <= 0x1A && key_id != KEY_CTRL_C)
-        {
-            /* Ignore the key Ctrl + A -> Z. */
-        }
-        else if ((KEY_FUNCTION == key_id) || (KEY_Fn == key_id))
-        {
-            uint8_t key_func_origin[2] =
-            {
-                key_id, getch()
-            };
-            uint8_t key_func = exsh.func_parser(key_func_origin, 2);
-            uint8_t index = key_func - Esh_Null;
-            void *para = exsh.key_reg_table[index].para;
-            if (exsh.key_reg_table[index].func != NULL)
-            {
-                exsh.key_reg_table[index].func(key_func, para);
-            }
-        }
-        else
-        {
-            break;
-        }
+        char ch = __getch();
+        osStatus_t ret_os = osMessageQueuePut(mq_getch, &ch, 0, 0);
+        assert(ret_os == osOK);
+    }
+}
+
+/**
+  * @brief  getch function for Linux.
+  * @retval Key id if > 0, and error id if < 0.
+  */
+static int getch(void)
+{
+    char ch;
+    int ret = 0;
+    osStatus_t ret_os = osMessageQueueGet(mq_getch, &ch, NULL, 20);
+    if (ret_os == osOK)
+    {
+        ret = ch;
+    }
+    else
+    {
+        ret = (int)ret_os;
     }
 
-    return key_id;
+    return ret;
 }
+#endif
+
+/* ----------------------------- end of file -------------------------------- */
--- eLab/midware/esh/esh.h
+++ eLab/midware/esh/esh.h
@@ -1,9 +1,23 @@
+/*
+ * eLab Project
+ * Copyright (c) 2023, EventOS Team, <event-os@outlook.com>
+ */
+
 #ifndef ESH_H
 #define ESH_H
 
+/* include ------------------------------------------------------------------ */
 #include <stdbool.h>
 #include "stdint.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* public define ------------------------------------------------------------ */
+/**
+  * @brief  The special key ID defines.
+  */
 enum esh_key_id
 {
     Esh_Null = 128,
@@ -14,15 +28,45 @@ enum esh_key_id
     Esh_Max
 };
 
-typedef void (* exsh_func_exit_t)(void *paras);
-typedef void (* exsh_func_t)(uint8_t key_id, void *paras);
-typedef uint8_t (* exsh_key_parser_t)(uint8_t *key, uint8_t count);
+/* public typedef ----------------------------------------------------------- */
+/**
+  * @brief  Key function type.
+  */
+typedef void (* esh_func_t)(uint8_t key_id, void *paras);
 
-void esh_init(exsh_key_parser_t func_parser,
-                exsh_func_t func,
-                exsh_func_exit_t func_exit,
-                void *paras);
-void esh_key_register(uint8_t key, exsh_func_t func, void *paras);
+/**
+  * @brief  Esh F-key (esh_key_id) parser function type.
+  */
+typedef uint8_t (* esh_key_parser_t)(uint8_t *value, uint8_t count);
+
+/* private function --------------------------------------------------------- */
+/**
+  * @brief  Esh F-key (esh_key_id) parser function type.
+  * @param  func_parser the F-key parser function.
+  * @retval None
+  */
+void esh_init(esh_key_parser_t func_parser);
+
+/**
+  * @brief  Esh F-key function registering.
+  * @param  key     f-key ID.
+  * @param  func    Ther F-key function.
+  * @param  paras   Parameters.
+  * @retval None
+  */
+void esh_key_register(uint8_t key, esh_func_t func, void *paras);
+
+/**
+  * @brief  Esh getch function.
+  * @param  None
+  * @retval key id.
+  */
 char esh_getch(void);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif
+
+/* ----------------------------- end of file -------------------------------- */
